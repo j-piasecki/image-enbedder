2b528f8abe86eca78d6e78810c723f3151a5be39
Kuba <jakubpiasecki67@gmail.com>
2025-03-09 14:20:19 +0100

    Decoding

--- src/main.rs
+++ src/main.rs
@@ -79,8 +79,8 @@ impl Iterator for MessageIter {
     }
 }
 
-fn load_image() -> Result<DynamicImage, Box<dyn std::error::Error>> {
-    let img = ImageReader::open("in.png")?.decode()?;
+fn load_image(path: &str) -> Result<DynamicImage, Box<dyn std::error::Error>> {
+    let img = ImageReader::open(path)?.decode()?;
     Ok(img)
 }
 
@@ -113,11 +113,63 @@ fn encode(onto: DynamicImage, text: &str, offsets: &[u32]) -> RgbaImage {
     result
 }
 
+fn to_bytes(message: Vec<bool>) -> Vec<u8> {
+    let mut result = Vec::new();
+    let mut byte = 0;
+
+    for (i, bit) in message.iter().enumerate() {
+        byte |= (*bit as u8) << (7 - i % 8);
+
+        if i % 8 == 7 {
+            result.push(byte);
+            byte = 0;
+        }
+    }
+
+    result
+}
+
+fn decode(from: DynamicImage, offsets: &[u32]) -> Option<String> {
+    let mut message = Vec::new();
+    let mut offset_iter = OffsetIter::new(offsets.to_vec());
+    let mut next_data_index = offset_iter.next().unwrap();
+
+    from.pixels().for_each(|pixel| {
+        let source_pixel = pixel.2;
+        let index = (pixel.0 + pixel.1 * from.dimensions().0) as usize;
+
+        if index == next_data_index {
+            message.push((source_pixel[0] & 1) == 1);
+            next_data_index = offset_iter.next().unwrap();
+        }
+    });
+
+    let bytes = to_bytes(message);
+    let length = unsafe {
+        u64::from_be(transmute([
+            bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
+        ]))
+    };
+    let message_bytes = &bytes[8..(8 + length as usize)];
+    let message = str::from_utf8(&message_bytes).unwrap();
+    Some(message.to_owned())
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(args);
 
-    let maybe_img = load_image();
+    // let maybe_img = load_image("in.png");
+
+    // if maybe_img.is_err() {
+    //     println!("Error loading image: {:?}", maybe_img.err());
+    //     return;
+    // }
+
+    // let source = maybe_img.unwrap();
+    // encode(source, "hello world", &[0]).save("out.png").unwrap();
+
+    let maybe_img = load_image("out.png");
 
     if maybe_img.is_err() {
         println!("Error loading image: {:?}", maybe_img.err());
@@ -125,5 +177,6 @@ fn main() {
     }
 
     let source = maybe_img.unwrap();
-    encode(source, "hello world", &[0]).save("out.png").unwrap();
+    let result = decode(source, &[0]).unwrap();
+    println!("Decoded message: {}", result);
 }
